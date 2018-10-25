c89e6aa41184042774b214fe4062f7df4be789c0
Arif <44020819+azimarif@users.noreply.github.com>
2018-10-25 11:42:12 +0530

    Implement makeCycler

--- src/lib.js
+++ src/lib.js
@@ -16,9 +16,27 @@ const makeCounterFromZero = function(){
     return counter++;
   }
 }
+
 const makeDeltaTracker = undefined;
+
 const makeFiboGenerator = undefined;
-const makeCycler = undefined;
+
+const makeCycler = function(array){
+  let index = 0;
+  let currentIndex =index;
+  myArray = array.slice();
+  length = myArray.length;
+  return function(){
+    if(index + 2 > length){
+      currentIndex = index;
+      index = 0;
+      return myArray[currentIndex];
+    }
+    currentIndex = index;
+    index++;
+    return myArray[currentIndex];
+  }
+};
 const curry = undefined;
 const compose = undefined;
 
